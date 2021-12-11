34586a6c27c73237ee0a05920612ab5c2de95ffe
Deepak Goyal <dg887180@gmail.com>
2021-12-11 11:15:19 +0530

    update style

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -8,130 +8,6 @@ function Projects(){
             <img src={projectImage} alt="projects.svg" />
             </div>
             <h1>My <span style={{color:"#63B4FF"}}>Projects</span></h1>
-
-            <section className="grey-section">
-            <h2 className="title">how well do you know me?</h2>
-            <small>Dec,2021</small>
-            <p>A CLI app built with NodeJS. You can send this to your friends and find out who really knows you</p>
-            <div className="container-fluid" >
-                <ul class="non-bullet">
-                    <li className="primary-btn"><a target="blank" href="https://replit.com/@dg8269/know-me?embed=1&output=1#index.js">Live Projects</a></li>
-                    <li className="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/know-me/blob/master/index.js">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            
-            <section className="white-section">
-            <h2 className="title">The Great Ramayan Quiz</h2>
-            <small>Dec,2021</small>
-            <p>A CLI app built with NodeJS. in which you can play quiz on Ramayan</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://replit.com/@dg8269/Ramayan-Quiz-1?embed=1&output=1">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Ramayan-Quiz-1">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="grey-section">
-            <h2 className="title">Banana Talks</h2>
-            <small>Dec,2021</small>
-            <p>A web app built with Vanilla JavaScript which convert english to minion language using api call.</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://banana-translattion-app.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/banana-translation-app">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="white-section">
-            <h2 className="title">Ebonics Translator</h2>
-            <small>Dec,2021</small>
-            <p>A web app built with Vanilla JavaScript which convert english to Ebonics language using api call.</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://ebonics-translator.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Ebonics-translator">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="grey-section">
-            <h2 className="title">Travel & Places Emoji Interpreter</h2>
-            <small>Dec,2021</small>
-            <p>A web app using React that show travel & places emojis and give its description</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://j3kbo.csb.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Travel-Places-Interpreter">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="white-section">
-            <h2 className="title">Music Rating App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using React which show ratings of favorite music</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://sccq7.csb.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/favorite-music-app">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-         
-            <section className="white-section">
-            <h2 className="title">Cash Register App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using Vanilla JavaScript enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://cash-register-manager-application.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/cash-register-app">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="grey-section">
-            <h2 className="title">Birthday Lucky App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using Vanilla JavaScript enter the birthdate and lucky number and show you are lucky or not ?</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://birthday-lucky-app.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Lucky-Birthday-app">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="white-section">
-            <h2 className="title">Fun With Triangle App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using Vanilla JavaScript which shows Angles of tirangles,Hypotaneuse,Area of Triangles and play with triangle quiz</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://fun-with-triangle-application.netlify.app/index.html">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Fun-with-Triangles">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="grey-section">
-            <h2 className="title">Palindrome Birthday App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using Vanilla JavaScript which check Palindrome birthday and that will also show the nearest palindrome date and how many days missed</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://palindrome-birthday-app.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Palindrome-birthday">View Source</a></li>
-                </ul>
-                </div>
-            </section>
-            <section className="white-section">
-            <h2 className="title">Stock Profit & Loss Calculator App</h2>
-            <small>Dec,2021</small>
-            <p>A web app using Vanilla JavaScript which shows stock profit and loss and also show the percentage of profit and loss.</p>
-            <div class="container-fluid">
-                <ul class="non-bullet">
-                    <li class="primary-btn"><a target="blank" href="https://profit-loss-calculator-app.netlify.app/">Live Projects</a></li>
-                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Profit-and-Loss-Calculator">View Source</a></li>
-                </ul>
-                </div>
-            </section>
             <section className="grey-section">
             <h2 className="title">Movie Trailer App</h2>
             <small>Dec,2021</small>
@@ -154,6 +30,133 @@ function Projects(){
                 </ul>
                 </div>
             </section>
+            <section className="grey-section">
+            <h2 className="title">Stock Profit & Loss Calculator App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using Vanilla JavaScript which shows stock profit and loss and also show the percentage of profit and loss.</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://profit-loss-calculator-app.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Profit-and-Loss-Calculator">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="white-section">
+            <h2 className="title">Palindrome Birthday App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using Vanilla JavaScript which check Palindrome birthday and that will also show the nearest palindrome date and how many days missed</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://palindrome-birthday-app.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Palindrome-birthday">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="grey-section">
+            <h2 className="title">Fun With Triangle App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using Vanilla JavaScript which shows Angles of tirangles,Hypotaneuse,Area of Triangles and play with triangle quiz</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://fun-with-triangle-application.netlify.app/index.html">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Fun-with-Triangles">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="white-section">
+            <h2 className="title">Birthday Lucky App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using Vanilla JavaScript enter the birthdate and lucky number and show you are lucky or not ?</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://birthday-lucky-app.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Lucky-Birthday-app">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="grey-section">
+            <h2 className="title">Cash Register App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using Vanilla JavaScript enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://cash-register-manager-application.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/cash-register-app">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="white-section">
+            <h2 className="title">Travel & Places Emoji Interpreter</h2>
+            <small>Dec,2021</small>
+            <p>A web app using React that show travel & places emojis and give its description</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a href="https://j3kbo.csb.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Travel-Places-Interpreter">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+           
+           
+            <section className="grey-section">
+            <h2 className="title">Music Rating App</h2>
+            <small>Dec,2021</small>
+            <p>A web app using React which show ratings of favorite music</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a href="https://sccq7.csb.app/">Live Projects</a></li>
+                    <li class="secondary-btn"><a  href="https://github.com/deepak29-git/favorite-music-app">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+            <section className="white-section">
+            <h2 className="title">Ebonics Translator</h2>
+            <small>Dec,2021</small>
+            <p>A web app built with Vanilla JavaScript which convert english to Ebonics language using api call.</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://ebonics-translator.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Ebonics-translator">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+         
+           
+            <section className="grey-section">
+            <h2 className="title">Banana Talks</h2>
+            <small>Dec,2021</small>
+            <p>A web app built with Vanilla JavaScript which convert english to minion language using api call.</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://banana-translattion-app.netlify.app/">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/banana-translation-app">View Source</a></li>
+                </ul>
+                </div>
+            </section>            
+            <section className="white-section">
+            <h2 className="title">The Great Ramayan Quiz</h2>
+            <small>Dec,2021</small>
+            <p>A CLI app built with NodeJS. in which you can play quiz on Ramayan</p>
+            <div class="container-fluid">
+                <ul class="non-bullet">
+                    <li class="primary-btn"><a target="blank" href="https://replit.com/@dg8269/Ramayan-Quiz-1?embed=1&output=1">Live Projects</a></li>
+                    <li class="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/Ramayan-Quiz-1">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+           
+            <section className="grey-section">
+            <h2 className="title">how well do you know me?</h2>
+            <small>Dec,2021</small>
+            <p>A CLI app built with NodeJS. You can send this to your friends and find out who really knows you</p>
+            <div className="container-fluid" >
+                <ul class="non-bullet">
+                    <li className="primary-btn"><a target="blank" href="https://replit.com/@dg8269/know-me?embed=1&output=1#index.js">Live Projects</a></li>
+                    <li className="secondary-btn" ><a target="blank" href="https://github.com/deepak29-git/know-me/blob/master/index.js">View Source</a></li>
+                </ul>
+                </div>
+            </section>
+
             <hr />
         </div>
     )
